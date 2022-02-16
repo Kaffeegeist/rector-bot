9e61b96997b3e9fb01b1995cbb2a90e876321c3f
Tch1b0 <Jpour3006@gmail.com>
2022-02-16 17:39:42 +0100

    added basic bot

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 A discord bot for pushing new notifications about our timetable
 
+## Setting up
+
+Create `.env` file in project root and create a `token` variable with the token as the value
+
 ## npm scripts
 
 Usage: `npm run <script>`
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-// Code execution starts here
+import { Client, Intents } from "discord.js";
+require("dotenv").config();
+
+const PREFIX = "r!";
+const client = new Client({
+    // GUILD = Discord Server
+    intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+});
+
+client.on("ready", () => {
+    console.log(client.user.tag);
+});
+
+client.on("message", async (message) => {
+    if (
+        message.author.id === client.user.id ||
+        !message.content.toLowerCase().startsWith(PREFIX)
+    )
+        return;
+
+    message.reply(`Du hast ${message.content} gesagt! Wie konntest du nur?`);
+});
+
+client.login(process.env.token);
