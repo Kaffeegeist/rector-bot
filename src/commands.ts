7a419d24b8913290f23f49170fa324197bd5044d
Tch1b0 <Jpour3006@gmail.com>
2022-02-18 18:50:45 +0100

    enhance `help` command

--- src/commands.ts
+++ src/commands.ts
@@ -1,9 +1,23 @@
+import { MessageEmbed } from "discord.js";
 import CommandHandler from "./commandhandler";
 
 const cmds = new CommandHandler();
 
-cmds.registerCommand("help", async (_client, message) => {
-    await message.reply("Hilfe");
-});
+cmds.registerCommand(
+    "help",
+    "displays this help box",
+    async (_client, message) => {
+        // create a new embed
+        const embed = new MessageEmbed().setColor("BLUE").setTitle("Help");
+
+        // add a new field for each command that is registered
+        cmds.commands.forEach((command) =>
+            embed.addField(`\`${command.name}\``, command.description),
+        );
+
+        // reply to the user with the embed
+        await message.reply({ embeds: [embed] });
+    },
+);
 
 export default cmds;
